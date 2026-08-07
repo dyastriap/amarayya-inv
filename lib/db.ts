d4b6b6c7aeb3a633cd45fef74044b54f9dec9c12
dyastriap <dyastriap12@gmail.com>
2026-08-07 09:44:48 +0700

    ganti server fb

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,12 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST, 
   user: process.env.DB_USER, 
   password: process.env.DB_PASSWORD, 
-  database: process.env.DB_NAME, 
+  database: process.env.DB_NAME,
+  port: 4000, // Port standar TiDB
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
